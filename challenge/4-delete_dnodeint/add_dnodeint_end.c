4a486beb08a64be78be5fed8c2b1c14ab02b7c7b
Rayan Hantoul <ryan-055@outlook.com>
2025-12-06 20:48:47 +0300

    fix code

--- challenge/4-delete_dnodeint/add_dnodeint_end.c
+++ challenge/4-delete_dnodeint/add_dnodeint_end.c
@@ -1,43 +1,43 @@
+#include <string.h>
 #include <stdlib.h>
 #include "lists.h"
 
 /**
- * add_dnodeint_end - Add a node at the end of a doubly linked list
+ * add_dnodeint_end - Add a node at the end of a list
  *
  * @head: The address of the pointer to the first element of the list
  * @n: The number to store in the new element
  *
- * Return: A pointer to the new element, or NULL on failure
+ * Return: A pointer to the new element
  */
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
-    dlistint_t *new;
-    dlistint_t *last;
+	dlistint_t *new;
+	dlistint_t *l;
 
-    if (head == NULL)
-        return NULL;
-
-    new = malloc(sizeof(dlistint_t));
-    if (new == NULL)
-        return NULL;
-
-    new->n = n;
-    new->next = NULL;
-
-    if (*head == NULL) // Empty list
-    {
-        new->prev = NULL;
-        *head = new;
-        return new;
-    }
-
-    // Traverse to the end of the list
-    last = *head;
-    while (last->next != NULL)
-        last = last->next;
-
-    last->next = new;
-    new->prev = last;
-
-    return new;
+	if (head == NULL)
+	{
+		return (NULL);
+	}
+	new = malloc(sizeof(dlistint_t));
+	if (new == NULL)
+	{
+		return (NULL);
+	}
+	new->n = n;
+	new->next = NULL;
+	if (*head == NULL)
+	{
+		*head = new;
+		new->prev = NULL;
+		return (new);
+	}
+	l = *head;
+	while (l->next != NULL)
+	{
+		l = l->next;
+	}
+	l->next = new;
+	new->prev = l;
+	return (new);
 }
--- challenge/4-delete_dnodeint/free_dlistint.c
+++ challenge/4-delete_dnodeint/free_dlistint.c
@@ -1,26 +1,20 @@
+#include <string.h>
 #include <stdlib.h>
 #include "lists.h"
 
 /**
- * free_dlistint - Free a doubly linked list
+ * free_dlistint - Free a list
  *
  * @head: A pointer to the first element of the list
  */
-void free_dlistint(dlistint_t **head)
+void free_dlistint(dlistint_t *head)
 {
-    dlistint_t *node;
-    dlistint_t *tmp;
+	dlistint_t *node;
 
-    if (head == NULL || *head == NULL)
-        return;
-
-    node = *head;
-    while (node)
-    {
-        tmp = node->next;
-        free(node);
-        node = tmp;
-    }
-
-    *head = NULL;  /* Avoid dangling pointer */
+	while (head)
+	{
+		node = head;
+		head = head->next;
+		free(node);
+	}
 }
